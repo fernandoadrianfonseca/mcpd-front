f7ec64820e34b15015390435d100a84bddf9fd64
Fernando Fonseca <fernandoadrianfonseca@gmail.com>
2025-05-20 19:10:41 -0300

    Agregadas entradas dinamicas para evitar problemas de hashes repetidos en los builds.

---
+++ src/dynamic-entry.ts
@@ -0,0 +1,4 @@
+import './app/components/main/main.component'
+import './app/components/login/login.component'
+import './app/components/stock-form/stock-form.component'
+import './app/components/proveedor-form/proveedor-form.component'
