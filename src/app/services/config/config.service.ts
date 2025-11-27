46a82d454f1113af42bd573322a1398c5b7f3cac
fernandoadrianfonseca <fernandoadrianfonseca@gmail.com>
2025-11-27 16:32:15 -0300

    Si es IP local redirigir a IP local los endpoints

--- src/app/services/config/config.service.ts
+++ src/app/services/config/config.service.ts
@@ -15,6 +15,16 @@ export class ConfigService {
   }
 
   get apiUrl(): string {
+    const currentHost = window.location.origin;
+
+    // Si la app está siendo accedida desde 192.168.1.181:8080
+    if (currentHost === 'https://192.168.1.181:8080' ||
+        currentHost === 'http://192.168.1.181:8080') {
+
+      return `${currentHost}/mcpd`;
+    }
+
+    // Caso normal: usa el config.json
     return this.config.apiUrl || '';
   }
 }
